7013d28d3fa82ab6f2347ae4db19c73e28a1b39d
Renee Stephen <renee.stephen@pantheon.io>
2022-05-01 13:35:14 -0700

    Adding YAML Content install step.

--- web/private/scripts/drush_site_install/drush_install.sh
+++ web/private/scripts/drush_site_install/drush_install.sh
@@ -9,7 +9,7 @@ echo "Installing rain_demo profile...\n"
 drush site-install tyche_rain_gear install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL --verbose --yes --account-mail="$EMAIL" --site-name="$TITLE" --account-name superuser
 
 # echo "Importing default content...\n"
-# drush config-import --source="profiles/contrib/tyche_rain_gear/config/install" --yes
+drush ycip tyche_rain_gear
 
 echo "Clearing cache...\n"
 passthru('drush cache-rebuild');
