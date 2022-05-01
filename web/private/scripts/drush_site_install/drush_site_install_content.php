2a58a7c09dfee0d69452d2dc204c16b6839672dc
Renee Stephen <renee.stephen@pantheon.io>
2022-05-01 13:07:53 -0700

    Tweaking install scripts, adding packages so we can standalone the profile, adding missing module.

--- web/private/scripts/drush_site_install/drush_site_install_content.php
+++ web/private/scripts/drush_site_install/drush_site_install_content.php
@@ -1,9 +1,9 @@
 <?php
 
 // // Import config changes.
-// echo "Importing default content...\n";
-// passthru('drush config-import --source profiles/contrib/rain_demo/config/install -y');
+//echo "Importing default content...\n";
+//passthru('drush config-import --source profiles/contrib/tyche_rain_gear/config/install -y');
 
 // // Clear all cache
-// echo "Rebuilding cache.\n";
-// passthru('drush cr');
+echo "Rebuilding cache.\n";
+passthru('drush cr');
